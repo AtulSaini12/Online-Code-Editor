d3889fdc1ffeb71d4c529a71c552c5581b182148
Atul Saini <atulsaini9886@gmail.com>
2021-12-01 17:12:33 +0530

    developed basic html structure

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,28 @@
+import React from "react";
+
+import Editor from "./Editor";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* upper code section */}
+      <div className="upper-section">
+        <Editor /> {/** HTML code editor */}
+        <Editor /> {/** CSS code editor */}
+        <Editor /> {/** JS code editor */}
+      </div>
+      {/* lower visualisation section */}
+      <div className="lower-section">
+        <iframe
+          title="Result"
+          sandbox="allow-scripts"
+          frameBorder="1"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Editor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Editor() {
+  return (
+    <div>
+      <div>Code Editor</div>
+    </div>
+  );
+}
+
+export default Editor;
