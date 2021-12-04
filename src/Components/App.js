86602f4210e1fc4e1780ae182f68ddc276e3c4fa
Atul Saini <atulsaini9886@gmail.com>
2021-12-04 23:39:42 +0530

    Added Live Changes and editor functionality to view changes on iframe

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Editor from "./Editor";
 
 function App() {
-  const [html, setHtml] = useState("hi i am ATul Saini");
+  const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [codeDoc, setCodeDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCodeDoc(`
+          <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${js}</script>
+          </html>
+      `);
+    }, 200);
+
+    //when we change our codeDocs in the meantime of these 350 milliseconds, we will cancel our previous update
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <div className="App">
@@ -38,7 +54,7 @@ function App() {
         <iframe
           title="Result"
           sandbox="allow-scripts"
-          // frameBorder="1"
+          srcDoc={codeDoc}
           width="100%"
           height="100%"
         ></iframe>
