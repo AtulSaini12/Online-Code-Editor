2c991ae049c9ca37dfb37f0e5900d3579317b108
Atul Saini <atulsaini9886@gmail.com>
2021-12-04 23:55:29 +0530

    Added the expand collapse editor functionality to the button

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 //importing the required codemirror libraries
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
@@ -7,17 +7,35 @@ import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpandAlt, faCompressAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Editor({ displayName, language, value, onChange }) {
+  const [open, setOpen] = useState(true);
+
   const handleBeforeChange = (editor, data, value) => {
     onChange(value);
   };
 
+  const handleOpenClose = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "closed"}`}>
       <div className="editor-heading">
         {displayName}
-        <button>O/C</button>
+        <button
+          className={`shrink-expand-btn ${open ? "close-btn" : "open-btn"}`}
+          type="button"
+          onClick={handleOpenClose}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         className="code-mirror-wrapper"
